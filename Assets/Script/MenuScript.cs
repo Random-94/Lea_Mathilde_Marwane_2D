39e7beecb5b60f8ce7535877abbac06dc2dc6084
Fab <fallermagie@yahoo.fr>
2020-06-08 16:50:03 +0200

    Commentaires de code

--- Assets/Script/MenuScript.cs
+++ Assets/Script/MenuScript.cs
@@ -5,19 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+  //Chaque fonction représente un bouton du Menu dans la hierarchy des gameobjets
+  //(Design et disposition du code)
+
+
+  //Fonction pour le bouton "jouer"
   public void playBouton()
   {
-      SceneManager.LoadScene("SampleScene");
+      SceneManager.LoadScene("SampleScene");//Charge la scène du jeu
   }
 
+  //Fonction pour le bouton "Crédits"
   public void creditsBouton()
   {
-    SceneManager.LoadScene("FabCredits");
+    SceneManager.LoadScene("FabCredits");//Charge la scène de crédits
   }
 
+  //Fonction pour le bouton "Quitter"
   public void quitBouton()
   {
-      Debug.Log("Ferme le jeu");
-      Application.Quit();
+      Debug.Log("Ferme le jeu");//Code de débug pour voir si le bouton réagit bien.
+      Application.Quit();//Ferme et Arrête l'application
   }
 }
